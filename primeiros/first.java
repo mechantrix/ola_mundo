29cc5d9aaca39ec1aaa8e40b8e55baed6e3fe03c
mechantrix <47579102+mechantrix@users.noreply.github.com>
2020-04-12 13:15:31 +0100

    Update first.java

--- primeiros/first.java
+++ primeiros/first.java
@@ -4,6 +4,6 @@ import java.util.Stack;
 
 public class first {
    public static void main(String[] args) {
-       System.out.println("jesus salva");
+       System.out.println("Hello, Bitches");
    }
 }
